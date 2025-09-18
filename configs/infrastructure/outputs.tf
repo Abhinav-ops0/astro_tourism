2da8f84ee4e58eb584200ba179986ad53fce48dd
Abhinav-ops0 <Abhinav.Prakash@ops0.com>
2025-09-18 19:29:47 +0530

    Add Terraform infrastructure: I'll help you create an S3 bucket named "astro-tou...

---
+++ configs/infrastructure/outputs.tf
@@ -0,0 +1,22 @@
+# Outputs for the infrastructure
+# This file contains all output values from the Terraform configuration
+
+output "bucket_name" {
+  value       = aws_s3_bucket.astro_tour_bucket.id
+  description = "The name of the bucket"
+}
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.astro_tour_bucket.arn
+  description = "The ARN of the bucket"
+}
+
+output "bucket_domain_name" {
+  value       = aws_s3_bucket.astro_tour_bucket.bucket_domain_name
+  description = "The bucket domain name"
+}
+
+output "bucket_regional_domain_name" {
+  value       = aws_s3_bucket.astro_tour_bucket.bucket_regional_domain_name
+  description = "The bucket region-specific domain name"
+}
