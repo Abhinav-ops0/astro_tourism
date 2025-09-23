9e971e65579bc7d310a96aa7e683f6d752e2a8c2
Abhinav-ops0 <Abhinav.Prakash@ops0.com>
2025-09-23 15:25:33 +0530

    Update infrastructure files from IAC assistant

--- configs/infrastructure/variables.tf
+++ configs/infrastructure/variables.tf
@@ -4,7 +4,7 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "test-new-pr-format"
+  default     = "new-terraform-test-pr"
 }
 
 variable "environment" {
