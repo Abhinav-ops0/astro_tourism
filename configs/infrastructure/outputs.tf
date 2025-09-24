11446067887355c60fe30d9844f65c2bbaf36cf3
Abhinav-ops0 <Abhinav.Prakash@ops0.com>
2025-09-24 14:34:36 +0530

    Update infrastructure files from IAC assistant

--- configs/infrastructure/outputs.tf
+++ configs/infrastructure/outputs.tf
@@ -1,2 +1,12 @@
 # Outputs for the infrastructure
 # This file contains all output values from the Terraform configuration
+
+output "help_test_bucket_name" {
+  description = "The name of the help test bucket"
+  value       = aws_s3_bucket.help_test_bucket.id
+}
+
+output "help_test_bucket_arn" {
+  description = "The ARN of the help test bucket"
+  value       = aws_s3_bucket.help_test_bucket.arn
+}
